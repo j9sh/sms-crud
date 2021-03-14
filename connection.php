565db54aca8b1e212a3d9733d14ef3336a91aba0
j9sh <jash.rana0264@gmail.com>
2021-03-14 23:20:05 +0530

    database connection file

--- connection.php
+++ connection.php
@@ -1,17 +1,12 @@
 <?php
 
-    //mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mysql_dbname);
-    //DEFAULT username : root and password : n/a for PhpMyadmin
-    $link = mysqli_connect("localhost", "root", "", "sms");
+    // fill in your MYSQL details
+    $link = mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mysql_dbname);
 
+    // incase of an error
     if(mysqli_connect_error())
     {
         die("Error connecting to the database!");
     }
-/*
-    else
-    {
-        echo "Connected Successfully!";
-    }
-*/ 
+
 ?>
